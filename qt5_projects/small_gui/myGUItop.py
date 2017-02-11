2c9e6d4bd3f598df2524afb2f3ebea2adbd84a9b
carl <carl.wilkerson@gmail.com>
2017-02-10 21:44:36 -0800

    adding myGUItop.py

--- qt5_projects/small_gui/myGUItop.py
+++ qt5_projects/small_gui/myGUItop.py
@@ -1,58 +1,49 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 """
-ZetCode PyQt4 tutorial 
+ZetCode PyQt4 tutorial
 
-This program shows a confirmation 
+This program shows a confirmation
 message box when we click on the close
-button of the application window. 
+button of the application window.
 
 author: Jan Bodnar
-website: zetcode.com 
+website: zetcode.com
 last edited: October 2011
 """
 
-import sys
-import qtpy as QtGui
+from PyQt5.QtWidgets import (QApplication, QWidget, QMessageBox)
 
-print QtGui.API
 
-from qtpy import QtGui
+class Example(QWidget):
 
-class Example(QtGui.QWidget):
-    
-    def __init__(self):
-        super(Example, self).__init__()
-        
-        self.initUI()
-        
-        
-    def initUI(self):               
-        
-        self.setGeometry(300, 300, 250, 150)        
-        self.setWindowTitle('Message box')    
-        self.show()
-        
-        
+    def __init__(self, *args, **kwargs):
+        super(Example, self).__init__(*args, **kwargs)
+
+        self.init_ui()
+
+    def init_ui(self):
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Message box')
+
     def closeEvent(self, event):
         
-        reply = QtGui.QMessageBox.question(self, 'Message',
-            "Are you sure to quit?", QtGui.QMessageBox.Yes | 
-            QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+        reply = QMessageBox.question(QMessageBox(), 'Message',
+                                     "Are you sure to quit?", QMessageBox.Yes |
+                                     QMessageBox.No, QMessageBox.No
+                                     )
 
-        if reply == QtGui.QMessageBox.Yes:
+        if reply == QMessageBox.Yes:
             event.accept()
         else:
+            print 'cancel was clicked'
             event.ignore()        
-        
-        
-def main():
-    
-    app = QtGui.QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
-
 
 if __name__ == '__main__':
-    main()
+    import sys
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
