64317205cf5fe995783151c7ebfdc9c8d4cab12d
Carl Wilkerson <cwilkerson@inphi.com>
2017-02-10 18:31:59 -0800

    clean up

--- xmlrpc/xmlrpc_server.py
+++ xmlrpc/xmlrpc_server.py
@@ -1,6 +1,7 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
 
+
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
@@ -14,10 +15,12 @@ server.register_introspection_functions()
 # pow.__name__ as the name, which is just 'pow'.
 server.register_function(pow)
 
+
 # Register a function under a different name
-def adder_function(x,y):
+def adder_function(x, y):
     return x + y
 server.register_function(adder_function, 'add')
+
 
 # Register an instance; all the methods of the instance are
 # published as XML-RPC methods (in this case, just 'div').
