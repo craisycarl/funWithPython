2dddf4af38531c4cd7e8def2a18d34b0f9929461
carl <carl.wilkerson@gmail.com>
2017-02-20 00:16:42 -0800

    cleaned up class example even more

--- General/class_var.py
+++ General/class_var.py
@@ -1,35 +1,35 @@
-class MotherClass(object):
+class ParentClass(object):
     def __init__(self):
-        print 'mother class just got called'
+        print 'Parent class just got called'
         self.instance_var_parent = 49
 
 
-class MyClass(MotherClass, object):
+class ChildClass(ParentClass, object):
     def __init__(self):
-        super(MyClass, self).__init__()
-        print 'MyClass class just got called', self.instance_var_parent
+        super(ChildClass, self).__init__()
+        print 'Child class just got called', self.instance_var_parent
         self.instance_var_child = 0
 
     class_variable = 4
 
     @staticmethod
     def set_class_var(value):
-        MyClass.class_variable = value
+        ChildClass.class_variable = value
 
     @staticmethod
     def get_class_var():
-        return MyClass.class_variable
+        return ChildClass.class_variable
 
-C = MyClass()  # Create an instance of the MyClass object
+C = ChildClass()  # Create an instance of the MyClass object
 print C.instance_var_child  # What is the value of 'instance_var_child'?
 print C.class_variable  # What is the value of class_variable?
 C.set_class_var(10)  # Using 'set_class_var', write a new value.
-print MyClass.class_variable  # Print that new value.
+print ChildClass.class_variable  # Print that new value.
 
-D = MyClass()  # Create a new instance of the MyClass object
-print MyClass.class_variable  # What is the value of class_variable? Still 4?
+D = ChildClass()  # Create a new instance of the MyClass object
+print ChildClass.class_variable  # What is the value of class_variable? Still 4?
 D.set_class_var(555)  # Using 'set_class_var', write a new value.
-print MyClass.class_variable  # What is the value of class_variable?
+print ChildClass.class_variable  # What is the value of class_variable?
 print D.get_class_var()  # What is the value of class_variable using the function 'get_class_var'
 D.instance_var_child = 100  # Set the instance variable
 print D.instance_var_child  # What is the updated instance variable?
