eca84c7499ad4b8e738f782d11c907a8f7e1958e
Carl Wilkerson <cwilkerson@inphi.com>
2017-08-04 17:17:29 -0700

    add a new general python example for abstract base classes

---
+++ General/abstract_base_class_example.py
@@ -0,0 +1,27 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Abstract:
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        pass
+
+    def use_concrete_implementation(self):
+        print(self._concrete_method())
+
+    @abstractmethod
+    def _concrete_method(self):
+        pass
+
+
+class Concrete(Abstract):
+    def my_fuc(self):
+        return 3
+
+    # def _concrete_method(self):
+    #     return 2 * 3
+
+my_abc = Concrete()
+
+my_abc.use_concrete_implementation()
